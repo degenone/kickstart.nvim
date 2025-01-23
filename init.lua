c8dda0f3e724eb27b2f11358b6f50c36dcf42cfd
Tero Kilpeläinen <degenone1@gmail.com>
2025-01-23 12:56:17 +0200

    Remove python virtual env plugin

--- init.lua
+++ init.lua
@@ -104,28 +104,6 @@ require('lazy').setup({
     },
   },
 
-  {
-    -- A plugin to help you select a Python virtual environment.
-    'linux-cultist/venv-selector.nvim',
-    opts = {
-      name = 'venv',
-    },
-    dependencies = {
-      'neovim/nvim-lspconfig',
-      -- 'mfussenegger/nvim-dap',
-      -- 'mfussenegger/nvim-dap-python', --optional
-      { 'nvim-telescope/telescope.nvim', branch = '0.1.x', dependencies = { 'nvim-lua/plenary.nvim' } },
-    },
-    lazy = false,
-    branch = 'regexp', -- This is the regexp branch, use this for the new version
-    config = function()
-      require('venv-selector').setup()
-    end,
-    keys = {
-      { ',v', '<cmd>VenvSelect<cr>' },
-    },
-  },
-
   -- which-key to show you pending keybinds.
 
   { -- Fuzzy Finder (files, lsp, etc)
