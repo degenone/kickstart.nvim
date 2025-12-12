6c4f580eb23be7f321c110a88b660a21e8cf4ddb
Tero Kilpeläinen <degenone1@gmail.com>
2025-12-12 18:22:49 +0200

    Add eslint_d to linting

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -7,6 +7,11 @@ return {
       local lint = require 'lint'
       lint.linters_by_ft = lint.linters_by_ft or {}
       lint.linters_by_ft['python'] = { 'flake8' }
+      lint.linters_by_ft['javascript'] = { 'eslint_d' }
+      lint.linters_by_ft['javascriptreact'] = { 'eslint_d' }
+      lint.linters_by_ft['typescript'] = { 'eslint_d' }
+      lint.linters_by_ft['typescriptreact'] = { 'eslint_d' }
+      lint.linters_by_ft['vue'] = { 'eslint_d' }
 
       lint.linters_by_ft['clojure'] = nil
       lint.linters_by_ft['dockerfile'] = nil
