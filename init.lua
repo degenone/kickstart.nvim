f25918d81f0ce65c0568be597822c6295a7837fd
Tero Kilpeläinen <degenone1@gmail.com>
2026-02-28 23:05:18 +0200

    Add explorer opening command
    It does not work currently, so commented out

--- init.lua
+++ init.lua
@@ -76,6 +76,43 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+local function open_in_file_manager(select_file)
+  local path = select_file and vim.fn.expand '%:p' or vim.fn.expand '%:p:h'
+  local sys = vim.loop.os_uname().sysname
+
+  if sys == 'Windows_NT' then
+    if select_file then
+      vim.fn.system { 'explorer', '/select,"' .. path .. '"' }
+    else
+      vim.fn.system { 'explorer', '"' .. path .. '"' }
+    end
+  elseif sys == 'Linux' then
+    -- Try xdg-open (works on most distros)
+    vim.fn.system { 'xdg-open', path }
+  elseif sys == 'Darwin' then
+    -- macOS Finder
+    if select_file then
+      vim.fn.system { 'open', '-R', path }
+    else
+      vim.fn.system { 'open', path }
+    end
+  else
+    print('Unsupported OS: ' .. sys)
+  end
+end
+
+vim.api.nvim_create_user_command('OpenInFM', function()
+  open_in_file_manager(false)
+end, {})
+
+vim.api.nvim_create_user_command('RevealInFM', function()
+  open_in_file_manager(true)
+end, {})
+
+-- Not working rn.
+-- vim.keymap.set('n', '<leader>e', ':OpenInFM<CR>', { desc = 'Open in File Manager', silent = true })
+-- vim.keymap.set('n', '<leader>E', ':RevealInFM<CR>', { desc = 'Reveal in File Manager', silent = true })
+
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.uv.fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
