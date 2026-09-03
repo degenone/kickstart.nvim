7b99ef6086d841e59ea3515425723b995464d684
Tero Kilpeläinen <degenone1@gmail.com>
2026-09-03 21:46:42 +0300

    fix: remove Neovim deprecation warnings

--- lua/keymaps/diagnostics.lua
+++ lua/keymaps/diagnostics.lua
@@ -11,5 +11,13 @@ vim.diagnostic.config {
   underline = { severity = { min = vim.diagnostic.severity.WARN } },
   virtual_text = true,
   virtual_lines = false,
-  jump = { float = true },
+  jump = {
+    on_jump = function(_, bufnr)
+      vim.diagnostic.open_float {
+        bufnr = bufnr,
+        scope = 'cursor',
+        focus = false,
+      }
+    end,
+  },
 }
