c359ae337a9f38996d434b45723964295978a9fb
Tero Kilpeläinen <degenone1@gmail.com>
2026-09-04 19:52:33 +0300

    fix: lazy-load DAP commands

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -9,6 +9,15 @@
 return {
   -- NOTE: Yes, you can install new plugins here!
   'mfussenegger/nvim-dap',
+  cmd = {
+    'DapContinue',
+    'DapToggleBreakpoint',
+    'DapStepInto',
+    'DapStepOver',
+    'DapStepOut',
+    'DapTerminate',
+    'DapToggleRepl',
+  },
   -- NOTE: And you can specify dependencies as well
   dependencies = {
     -- Creates a beautiful debugger UI
